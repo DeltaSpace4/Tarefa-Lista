19e1c365fb3e490b4ac79dde5864f8539fba70fc
DeltaSpace4 <151398000+DeltaSpace4@users.noreply.github.com>
2024-05-29 18:09:41 -0300

    Add files via upload

---
+++ ListaTarefa.js
@@ -0,0 +1,62 @@
+let myNodelist = document.getElementsByTagName("li");
+for (let i = 0; i < myNodelist.length; i++) {
+let span = document.createElement("span");
+let txt = document.createTextNode("\u00D7"); //caracter x
+span.className = "close";
+span.appendChild(txt);
+myNodelist[i].appendChild(span);
+}
+
+let close = document.getElementsByClassName("close");
+for (let i = 0; i < close.length; i++) {
+close[i].onclick = function() {
+let div = this.parentElement;
+div.style.display = "none";
+}
+}
+
+let list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+if (ev.target.tagName === 'LI') {
+ev.target.classList.toggle('checked');
+}
+}, false);
+
+function addElemento() {
+let li = document.createElement("li");
+let inputValue = `${day}/${month}/${year} ` + document.getElementById("tarefa").value.toUpperCase();
+let t = document.createTextNode(inputValue);
+li.appendChild(t);
+if (inputValue === '') {
+alert("Você precisa descrever a tarefa");
+} else {
+document.getElementById("itemLista").appendChild(li);
+}
+document.getElementById("tarefa").value = "";
+let span = document.createElement("SPAN");
+let txt = document.createTextNode("\u00D7");
+span.className = "close";
+span.appendChild(txt);
+li.appendChild(span);
+for (let i = 0; i < close.length; i++) {
+close[i].onclick = function() {
+let div = this.parentElement;
+div.style.display = "none";
+}
+}
+}
+
+function deleteAll(){
+let div = document.getElementById("itemLista");
+div.style.display = "none";
+}
+
+function marcaAll(){
+let div = document.querySelectorAll("li");
+div.forEach(div.classList.toggle('checked'));
+}
+
+const date = new Date();
+const year = date.getFullYear();
+const month = String(date.getMonth() + 1).padStart(2, '0');
+const day = String(date.getDate()).padStart(2, '0');
